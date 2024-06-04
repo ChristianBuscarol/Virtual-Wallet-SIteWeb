636def1a6ee03385b6498b927d99ea3c89a19255
Christian Buscarol <Chris-Emanuel-B@outlook.com>
2024-06-03 21:32:15 -0300

    Corregido el error respecto al ingreso de datos del usuario

--- src/components/EntryForm.vue
+++ src/components/EntryForm.vue
@@ -6,9 +6,9 @@
     <input type="text" v-model="userId" id="registroId" name="ingresoID" placeholder="Ingresar ID aquí..."><br><br>
     <button type="button" @click="btnDateValidation()" :disabled="entryAttemptsFalied()" id="btnValidarIngreso">Validar Datos...</button>
     <div class="Casilla-Loggin">
-      <p><strong>Estado de entrada: '{{ stateMessage }}'<br><br>
-        Elementos del ingreso del 'ID': Cant. de carácteres '°{{idLetters}}' & Cant. de números '°{{ idNumbers }}'
-      </strong></p><br><br>
+      <p><strong>Estado de entrada: '{{ stateMessage }}'
+        <br>Elementos del ingreso del 'ID': Cant. de carácteres '°{{idLetters}}' & Cant. de números '°{{ idNumbers }}'
+      </strong></p><br>
       <p>Intentos realizados: °{{ attemptIncremented }} --- Cantidad máxima de intentos: °3</p>
       <p v-show="vShowMessage">Se ha realizado todos los intentos disponibles para poder continuar por el sitio, recargue la página para volver a intentar...</p>
       <button type="button" @click="btnEntryValidation()" :disabled="entryAttemptsFalied()" id="btnValidarIngreso">Puede continuar por aquí...</button>
@@ -34,50 +34,51 @@ export default {
   },
   methods: {
     nameValidation() {
+      debugger
       for (let i = 0; i < this.userName.length; i++){
-        const charName = this.userName[i]
+        let charName = this.userName[i]
 
-        if(!isNaN(Number(charName))){this.nameNumbers += 1}
-        else if(/[a-zA-Z]/.test(charName)){this.nameLetters += 1}
+        if(!isNaN(Number(charName))){this.nameNumbers++}
+        else if(/[a-zA-Z]/.test(charName)){this.nameLetters++}
       }
     },
     IDValidation() {
+      debugger
       for (let i = 0; i < this.userId.length; i++){
-        const charId = this.userId[i]
+        let charId = this.userId[i]
 
-        if(!isNaN(Number(charId))){this.idNumbers += 1}
-        else if(/[a-zA-Z]/.test(charId)){this.idLetters += 1}
+        if(!isNaN(Number(charId))){this.idNumbers++}
+        else if(/[a-zA-Z]/.test(charId)){this.idLetters++}
       }
     },
     btnDateValidation(){
+      debugger
       this.nameValidation();
       this.IDValidation();
-
-      do{
-        if(this.userId == "" || this.userName == ""){
+      
+      if(this.userId == "" || this.userName == ""){
           this.stateMessage= "Uno o todos los campos de la entrada de datos a quedado vacío, por favor, revise e ingrese cada dato que se solicite...",
           this.attemptIncrement()
         }
-        else{
-          if (this.nameNumbers > 0 || this.nameLetters < 3){
-            this.stateMessage= "El 'Nombre' ingresado es incorrecto, el dato requiere de un mínimo de 3 letras y no se permite números en el...";
-            this.attemptIncrement()
-          }
-          else if (this.idLetters < 5 || this.nameNumbers >= 1){
-            this.stateMessage= "El 'ID' está flojo de papeles, se requiere de un mínimo de 3 letras y al menos 1 número, intente de nuevo...";
-            this.attemptIncrement()
-          }
-          else if (this.nameNumbers >= 1 || this.nameLetters < 3 && this.idLetters < 5 || this.idNumbers > 1){
-            this.stateMessage= "Uno o ambos datos ingresados ha sido de manera icorrecta, intente de nuevo..."
-            this.attemptIncrement()
-          }
-          else{
-            this.stateMessage= "Felicitaciones!!!... Supongo...Cada dato solicitado ha sido ingresado correctamente, así que sea bienvenido/a a continuar por el sitio web y también lo invito a no asustarse por el precio de las Criptos...",
-            this.userId = "",
-            this.userName = ""
-          }
+      else{
+        if (this.nameNumbers > 0 || this.nameLetters < 3){
+          this.stateMessage= "El 'Nombre' ingresado es incorrecto, el dato requiere de un mínimo de 3 letras y no se permite números en el...";
+          this.attemptIncrement()
         }
-      }while(this.entryAttempts < 3)
+        else if (this.idLetters < 5 || this.idNumbers == 0){
+          this.stateMessage= "El 'ID' está flojo de papeles, se requiere de un mínimo de 3 letras y al menos 1 número, intente de nuevo...";
+          this.attemptIncrement()
+        }
+        else if (this.nameNumbers >= 1 || this.nameLetters <= 2 && this.idLetters <= 4 || this.idNumbers < 1){
+          this.stateMessage= "Uno o ambos datos ingresados ha sido de manera icorrecta, intente de nuevo..."
+          this.attemptIncrement()
+        }
+        else{
+          this.stateMessage= "Felicitaciones!!!... Supongo...Cada dato solicitado ha sido ingresado correctamente, así que sea bienvenido/a a continuar por el sitio web y también lo invito a no asustarse por el precio de las Criptos...",
+          this.userId = "",
+          this.userName = ""
+        }
+      }
     },
     btnEntryValidation(){
       window.location.href = '/CoinCatalogueView';
@@ -87,10 +88,10 @@ export default {
     },
     entryAttemptsFalied(){
       if (this.entryAttempts == 3){
-        return this.dateValidation = true;
+        this.dateValidation = true;
       }
       else{
-        return this.dateValidation = false;
+        this.dateValidation = false;
       }
     }
   },
