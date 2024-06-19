74c4e578a79d06e6d8497307a5af74aac529260c
Christian Buscarol <Chris-Emanuel-B@outlook.com>
2024-06-19 12:50:54 -0300

    Se creado dicho componente para el perfil de usuario, apenas tiene unas propiedades en Data, se necesita rellenar con más datos e info, importarse e interactuar con otros comonentes

---
+++ src/components/UserProfileComponent.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<script>
+export default{
+  name: 'UserProfileComponent',
+  data(){
+    return{
+      userName: '',
+      userId: '',
+      userWallet: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
