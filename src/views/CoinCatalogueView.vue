bdea47a4ce28c19b714e94ec337b7c71f3781660
Christian Buscarol <Chris-Emanuel-B@outlook.com>
2024-06-19 22:07:31 -0300

    Se ha modificado levemente esta vista acomodándolo para futuras importaciones de componentes y otros cambios que se aplicarían en su template y Css.

--- src/views/CoinCatalogueView.vue
+++ src/views/CoinCatalogueView.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="catalogue">
-    <h1>This is an catalogue page</h1>
+    <header>
+      <div class="CoinCatalogue">
+        <h1>This is an catalogue page</h1>
+      </div>
+    </header>
+    
+    <footer>
+      <div>
+
+      </div>
+    </footer>
   </div>
 </template>
+
+<style scoped>
+
+</style>
