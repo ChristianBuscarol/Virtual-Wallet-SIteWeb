d23b0cadd1ead5c7b8d0a1561136b39f7ad73a14
Christian Buscarol <Chris-Emanuel-B@outlook.com>
2024-06-19 22:10:52 -0300

    Apenas se ha tocado este componente, y dichas modificaciones en su estructura en general son insignificantes. Al final y al cabo hice este commit para que la terminal de Git no me ande hirviendo las frutas.

--- src/components/UserProfileComponent.vue
+++ src/components/UserProfileComponent.vue
@@ -1,18 +1,22 @@
 <template>
-
+  <div class="UserProfile">
+    <body>
+      
+    </body>
+  </div>
 </template>
 
 <script>
-export default{
-  name: 'UserProfileComponent',
-  data(){
-    return{
-      userName: '',
-      userId: '',
-      userWallet: 0
+  export default{
+    name: 'UserProfileComponent',
+    data(){
+      return{
+        userName: '',
+        userId: '',
+        userWallet: 0
+      }
     }
   }
-}
 </script>
 
 <style scoped>
