4ce48d93714a945ec76876c5a5f9ac2e85f1eb56
Christian Buscarol <Chris-Emanuel-B@outlook.com>
2024-07-05 16:49:28 -0300

    Se agregó esta vista para renderizar en formato de tabla, o quizás de lista, el historial de compra y venta de las monedas realizadas por el usuario.

---
+++ src/views/UsuaryHistoryView.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="HistoryHeaderBox">
+    <h3>Records table of the usuary: </h3>
+  </header>
+  <div>
+
+  </div>
+  <footer class="HistoryFooterBox">
+
+  </footer>
+</template>
+
+<script>
+
+
+  export default {
+    name: 'UsuaryHistoryView',
+    components: {
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
