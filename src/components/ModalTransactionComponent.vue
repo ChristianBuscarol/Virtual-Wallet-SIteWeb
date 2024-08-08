bbdc325335a9f2c3662c931e6df8f150a88d7482
Christian Buscarol <Chris-Emanuel-B@outlook.com>
2024-08-07 21:53:30 -0300

    Se estuvo manipulando y mejorando el componente del Modal para la transacción de la moneda seleccionada, agregando estructura a su template y datos y funciones en su script para ser abierto y cerrado dicho Modal, aunque esta cosa todavía no funciona, tengo que revisar cómo puedo trabajar en la importación de dicho componente y en la escucha de eventos y datos a recibir para su correcto funcionamiento.

--- src/components/ModalTransactionComponent.vue
+++ src/components/ModalTransactionComponent.vue
@@ -1,15 +1,50 @@
 <template>
   <body>
-    <div class="ModalRendering">
-
+    <div v-if="modalVisibility" class="ModalRendering">
+      <h3>Coin selected for the transaction: </h3>
+      <h3>Type of transaction</h3>
+      <h3>Coin image display:</h3>
+      <img src="" alt="GifSelectedCoin" class="Coin-Circle">
+      <h3>Price</h3>
+      <button type="button" @click="closeModalTransaction()" id="btnValidateSale">Cancel and close this transaction window.</button>
     </div>
   </body>
 </template>
 
 <script>
+  import { eventBus } from '@/utility/eventBusUtility';
 
+  export default{
+    name: 'ModalTransactionComponent',
+    data(){
+      return{
+        modalVisibility: false
+      }
+    },
+    methods: {
+      showModalTransaction(){
+        this.modalVisibility = true;
+      },
+      closeModalTransaction(){
+        this.modalVisibility = false;
+      }
+    },
+    mounted(){
+      eventBus.$on('open-transaction-modal', this.showModalTransaction);
+    },
+    beforeDestroy(){
+      eventBus.$off('open-transaction-modal', this.showModalTransaction);
+    }
+  }
 </script>
 
 <style scoped>
-
+  .Coin-Circle{
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    border: 2px;
+    border-style: groove;
+    border-color: black;
+  }
 </style>
