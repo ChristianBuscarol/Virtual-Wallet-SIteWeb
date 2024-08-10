9afb5eabb88342dc219fb9c2f0c59ef6c481defd
Christian Buscarol <Chris-Emanuel-B@outlook.com>
2024-08-09 22:03:00 -0300

    Se recibió el objeto con la información de la moneda seleccionada del componente del cataálogo de monedas y también se manipula la variable booleana encargada de hacer visible el modal, pero por algún motivo no se logra visualizar el modal mismo, capaz le esté errando en alguna tontería, hay que seguir viendo y probando.

--- src/components/ModalTransactionComponent.vue
+++ src/components/ModalTransactionComponent.vue
@@ -1,12 +1,14 @@
 <template>
   <body>
-    <div v-if="modalVisibility" class="ModalRendering">
-      <h3>Coin selected for the transaction: </h3>
-      <h3>Type of transaction</h3>
-      <h3>Coin image display:</h3>
-      <img src="" alt="GifSelectedCoin" class="Coin-Circle">
-      <h3>Price</h3>
-      <button type="button" @click="closeModalTransaction()" id="btnValidateSale">Cancel.</button>
+    <div v-if="modalVisibility == true" class="ModalRendering">
+      <div class="ModalContent" @click.stop>
+        <h3>Coin selected for the transaction: {{ this.infoSelectedCoinReceived.coinTittle }}</h3>
+        <h3>Type of transaction: </h3>
+        <h3>Coin image display:</h3>
+        <img src="" alt="GifSelectedCoin" class="Coin-Circle">
+        <h3>Price</h3>
+        <button type="button" @click="closeModalTransaction()" id="btnValidateSale">Cancel.</button>
+      </div>
     </div>
   </body>
 </template>
@@ -30,6 +32,7 @@
     methods: {
       showModalTransaction(){
         this.modalVisibility = true;
+        console.log(this.modalVisibility)
       },
       closeModalTransaction(){
         this.modalVisibility = false;
@@ -37,10 +40,12 @@
       transactionDataLoading(newVal){
         this.infoSelectedCoinReceived = newVal;
         console.log(this.infoSelectedCoinReceived);
+
+        this.showModalTransaction();
       }
     },
     watch: {
-      receivedData: {
+      receivedSelectedCoinInfo: {
         handler(newVal) {
           if(newVal){
             this.transactionDataLoading(newVal);
@@ -64,5 +69,35 @@
 
   .ModalRendering{
     display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.6); /* Fondo opaco */
+    opacity: 0;
+    visibility: hidden;
+    transition: opacity 0.5s ease-in-out, visibility 0.5s ease-in-out;
+  }
+
+  .ModalRendering.show{
+    opacity: 1;
+    visibility: visible;
+  }
+
+  .ModalContent{
+    background-color: #fff;
+    width: 33%; /* Ocupa un tercio de la pantalla */
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
+    transform: scale(1);
+    transition: transform 0.3s ease-in-out, opacity 0.3s ease-in-out;
+  }
+
+  .ModalContent.show{
+    transform: scale(1); /* Se expande a tamaño completo */
   }
 </style>
