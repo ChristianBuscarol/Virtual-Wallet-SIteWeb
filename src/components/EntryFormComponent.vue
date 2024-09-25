18b61a9cd24f9e7ea0fd9e7df01d1cfd232c156d
Christian Buscarol <Chris-Emanuel-B@outlook.com>
2024-09-24 22:11:36 -0300

    Se quitaron las funciones referidas a la consulta de la Api para saber las conexiones anteriores del usuario que se acaba de conectar en el sitio web, y se pasó todo ese código al componente del perfil de usuario para encapsular dichas funciones ahí mismo y que sean usadas mediante mensajes recibidos y emitidos por el resto de la plataforma.

--- src/components/EntryFormComponent.vue
+++ src/components/EntryFormComponent.vue
@@ -28,8 +28,6 @@
 </template>
 
 <script>
-  import ApiCallService from '@/services/ApiCallService';
-
   export default {
     name: 'EntryFormComponent',
     data() {
@@ -44,23 +42,9 @@
         dateValidation: true,
         entryAttempts: 0,
         localStorageComparison: 0,
-        historyOfUserMovementsTransactions: [],
-        historyOfPurchaseTransactions: [],
-        historyOfSaleTransactions: [],
         userData: {
           userNameRegister: "",
           userIdRegister: "",
-          userMoneyRegister: 0,
-          moneySpent: 0,
-          moneyEarned: 0,
-          coinAvailableList: {
-            bitcoinAmount: 0,
-            dogecoinAmount: 0,
-            ethereumAmount: 0,
-            litecoinAmount: 0,
-            solanaAmount: 0,
-            usdcAmount: 0
-          }
         }
       }
     },
@@ -117,126 +101,8 @@
         this.userData.userNameRegister = this.userName;
         this.userData.userIdRegister = this.userId;
       },
-      firstConnectionMoneyGift(){
-        // Este va a ser un pequeño regalo para el usuario en caso de conectarse por °1 vez y no haber hecho transacciones aún.
-        this.userData.userMoneyRegister = 1000000;
-      },
       userRegisterValidation(){
-        // Una vez los datos ingresados sean correctos, se llamará a este método, lo cual desencadenará una suceción de varios métodos más que investigarán anteriores conexiones del usuario en cuestión.
-        this.consultingApiForUserMovements();
         this.userObjectConstructor();
-      },
-      async consultingApiForUserMovements(){
-        // Esta función llamará a la Api para pedir la información del usuario requerida según el Id que le pasamos como parámetro y después, llamamos a otra función para analizar la información que conseguimos y a la vez le pasamos como parámetro también.
-        let response = await ApiCallService.getUserTransactionsInfo(this.userId);
-
-        this.evaluatingUserFirstConnection(response);
-      },
-      evaluatingUserFirstConnection(response){
-        if(response.data != null || response.data != undefined){
-          this.fillingUserHistoryArraySpace(response);
-          this.localStorageComparison = 1;
-        } else{
-          this.firstConnectionMoneyGift();
-          this.localStorageComparison = 2;
-        }
-      },
-      fillingUserHistoryArraySpace(response){
-        for(let i = 0; i < response.data.length; i++){
-          this.historyOfUserMovementsTransactions[i] = response.data[i];
-        }
-
-        this.separatingTransactionsByType();
-      },
-      separatingTransactionsByType(){
-        for(let i = 0; i < this.historyOfUserMovementsTransactions.length; i++){
-          if(this.historyOfUserMovementsTransactions[i].action == 'purchase'){
-            this.historyOfPurchaseTransactions.push(this.historyOfUserMovementsTransactions[i]);
-          }
-          else if(this.historyOfUserMovementsTransactions[i].action == 'sell'){
-            this.historyOfSaleTransactions.push(this.historyOfUserMovementsTransactions[i]);
-          }
-        }
-
-        this.calculatingUserMoneyAvailable();
-      },
-      calculatingUserMoneyAvailable(){
-        this.sumOfMoney();
-        this.restOfMoney();
-        this.totalUserAvailableMoney();
-      },
-      restOfMoney(){
-        for(let i = 0; i < this.historyOfPurchaseTransactions.length; i++){
-          this.userData.moneySpent = this.userData.moneySpent + parseFloat(this.historyOfPurchaseTransactions[i].money);
-        }
-      },
-      sumOfMoney(){
-        for(let i = 0; i < this.historyOfSaleTransactions.length; i++){
-          if(isNaN(this.historyOfSaleTransactions[i].money) != true){
-            this.userData.moneyEarned = this.userData.moneyEarned + parseFloat(this.historyOfSaleTransactions[i].money);
-            console.log(this.userData.moneyEarned);
-          }
-        }
-      },
-      totalUserAvailableMoney(){
-        this.userData.userMoneyRegister = this.userData.moneyEarned - this.userData.moneySpent;
-
-        this.calculatingUserCoinsAvailable();
-      },
-      calculatingUserCoinsAvailable(){
-        this.sumOfCoins();
-        this.restOfCoins();
-        this.localStorageSettingItems();
-      },
-      sumOfCoins(){
-        for(let i = 0; i < this.historyOfPurchaseTransactions.length; i++){
-          if(this.historyOfPurchaseTransactions[i].crypto_code == 'bitcoin'){
-            this.userData.coinAvailableList.bitcoinAmount += parseFloat(this.historyOfPurchaseTransactions[i].crypto_amount);
-          }
-          else if (this.historyOfPurchaseTransactions[i].crypto_code == 'dogecoin'){
-            this.userData.coinAvailableList.dogecoinAmount += parseFloat(this.historyOfPurchaseTransactions[i].crypto_amount);
-          }
-          else if (this.historyOfPurchaseTransactions[i].crypto_code == 'ethereum'){
-            this.userData.coinAvailableList.ethereumAmount += parseFloat(this.historyOfPurchaseTransactions[i].crypto_amount);
-          }
-          else if (this.historyOfPurchaseTransactions[i].crypto_code == 'litecoin'){
-            this.userData.coinAvailableList.litecoinAmount += parseFloat(this.historyOfPurchaseTransactions[i].crypto_amount);
-          }
-          else if (this.historyOfPurchaseTransactions[i].crypto_code == 'solana'){
-            this.userData.coinAvailableList.solanaAmount += parseFloat(this.historyOfPurchaseTransactions[i].crypto_amount);
-          }
-          else if (this.historyOfPurchaseTransactions[i].crypto_code == 'usdcd'){
-            this.userData.coinAvailableList.usdcAmount += parseFloat(this.historyOfPurchaseTransactions[i].crypto_amount);
-          }
-        }
-      },
-      restOfCoins(){
-        for(let i = 0; i < this.historyOfSaleTransactions.length; i++){
-          if(this.historyOfSaleTransactions[i].crypto_code == 'bitcoin'){
-            this.userData.coinAvailableList.bitcoinAmount -= parseFloat(this.historyOfSaleTransactions[i].crypto_amount);
-          }
-          else if (this.historyOfSaleTransactions[i].crypto_code == 'dogecoin'){
-            this.userData.coinAvailableList.dogecoinAmount -= parseFloat(this.historyOfSaleTransactions[i].crypto_amount);
-          }
-          else if (this.historyOfSaleTransactions[i].crypto_code == 'ethereum'){
-            this.userData.coinAvailableList.ethereumAmount -= parseFloat(this.historyOfSaleTransactions[i].crypto_amount);
-          }
-          else if (this.historyOfSaleTransactions[i].crypto_code == 'litecoin'){
-            this.userData.coinAvailableList.litecoinAmount -= parseFloat(this.historyOfSaleTransactions[i].crypto_amount);
-          }
-          else if (this.historyOfSaleTransactions[i].crypto_code == 'solana'){
-            this.userData.coinAvailableList.solanaAmount -= parseFloat(this.historyOfSaleTransactions[i].crypto_amount);
-          }
-          else if (this.historyOfSaleTransactions[i].crypto_code == 'usdcd'){
-            this.userData.coinAvailableList.usdcAmount -= parseFloat(this.historyOfSaleTransactions[i].crypto_amount);
-          }
-        }
-      },
-      localStorageSettingItems(){
-        // Una vez listo nuestro 'userData' se empuja dicho objeto al Local Storage.
-        localStorage.setItem('userData', JSON.stringify(this.userData))
-        
-        // Una vez los datos del usuario han sido consultados, confirmados y guardados en el Local Storage, se llama al método dedicado a emitir el evento para que ejecute dicha función.
         this.userRegister();
       },
       userRegister(){
