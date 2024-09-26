fe62b43a3d639d913010dd0dccb9fca3eee91ec0
Christian Buscarol <Chris-Emanuel-B@outlook.com>
2024-09-25 21:29:16 -0300

    Se agregaron unas funciones que se habían eliminado u olvidado por accidente al traer todo el código referido hacia la consulta de la Api para chequear actividad anterior del usuario en la plataforma, también se pilió un poco dichas funciones cambiando el nombre de algunas características, como hacer referencia al objeto que contiene toda la info (Variables) del usuario en cuestión, si bien se podría chequear que no falte nada e incluso agregar nuevas funciones, por ahora tocará probar y rezar que estos cambios traigan la menor cantidad de problemas posibles...

--- src/components/UserProfileComponent.vue
+++ src/components/UserProfileComponent.vue
@@ -9,6 +9,8 @@
 </template>
 
 <script>
+  import ApiCallService from '@/services/ApiCallService';
+
   export default{
     name: 'UserProfileComponent',
     props: {
@@ -29,9 +31,11 @@
           userWallet: 0,
           totalMoneySpent: 0,
           totalMoneyEarned: 0,
-          totalMoneyLost: 0,
           totalCoinsPurchased: 0,
           totalCoinsSold: 0,
+          historyOfUserMovementsTransactions: [],
+          historyOfPurchaseTransactions: [],
+          historyOfSaleTransactions: [],
           unitCoinAmount: {
             bitcoinAmount: 0,
             dogecoinAmount: 0,
@@ -47,70 +51,109 @@
       receiverEventData(newVal){
         this.dataUserProfile.userName = newVal.userNameRegister;
         this.dataUserProfile.userId = newVal.userIdRegister;
-        this.dataUserProfile.userWallet = newVal.userMoneyRegister;
-        this.dataUserProfile.totalMoneySpent = newVal.moneySpent;
-        this.dataUserProfile.totalMoneyEarned = newVal.moneyEarned;
-        this.dataUserProfile.unitCoinAmount= newVal.coinAvailableList;
+        this.consultingApiForUserMovements();
       },
-      receiptOfTransactionDoneInfo(newVol){
-        if(newVol.action == 'purchase'){
-          this.sumOfMoneyEarnedFromTransactionDone(newVol);
-          this.sumOfCoins(newVol);
+      async consultingApiForUserMovements(){
+        // Esta función llamará a la Api para pedir la información del usuario requerida según el Id que le pasamos como parámetro y después, llamamos a otra función para analizar la información que conseguimos y a la vez le pasamos como parámetro también.
+        let response = await ApiCallService.getUserTransactionsInfo(this.userId);
+
+        this.evaluatingUserFirstConnection(response);
+      },
+      evaluatingUserFirstConnection(response){
+        if(response.data != null || response.data != undefined){
+          this.fillingUserHistoryArraySpace(response);
+        } else{
+          this.firstConnectionMoneyGift();
         }
-        else if(newVol.action == 'sell'){
-          this.restOfMoneySpentFromTransactionDone();
-          this.restOfCoins(newVol);
+      },
+      firstConnectionMoneyGift(){
+        // Este va a ser un pequeño regalo para el usuario en caso de conectarse por °1 vez y no haber hecho transacciones aún.
+        this.dataUserProfile.userWallet = 1000000;
+      },
+      fillingUserHistoryArraySpace(response){
+        for(let i = 0; i < response.data.length; i++){
+          this.historyOfUserMovementsTransactions[i] = response.data[i];
+        }
+
+        this.separatingTransactionsByType();
+      },
+      separatingTransactionsByType(){
+        for(let i = 0; i < this.historyOfUserMovementsTransactions.length; i++){
+          if(this.historyOfUserMovementsTransactions[i].action == 'purchase'){
+            this.historyOfPurchaseTransactions.push(this.historyOfUserMovementsTransactions[i]);
+          }
+          else if(this.historyOfUserMovementsTransactions[i].action == 'sell'){
+            this.historyOfSaleTransactions.push(this.historyOfUserMovementsTransactions[i]);
+          }
+        }
+
+        this.calculatingUserMoneyAvailable();
+      },
+      calculatingUserMoneyAvailable(){
+        this.sumOfMoney();
+        this.restOfMoney();
+        this.totalUserAvailableMoney();
+      },
+      sumOfMoney(){
+        for(let i = 0; i < this.historyOfSaleTransactions.length; i++){
+          this.dataUserProfile.totalMoneyEarned = this.dataUserProfile.totalMoneyEarned + parseFloat(this.historyOfSaleTransactions[i].money);
         }
       },
-      sumOfMoneyEarnedFromTransactionDone(){
-        this.dataUserProfile.userWallet += this.newVol.money;
-        this.dataUserProfile.totalMoneyEarned += this.newVol.money;
+      restOfMoney(){
+        for(let i = 0; i < this.historyOfPurchaseTransactions.length; i++){
+          this.dataUserProfile.totalMoneySpent = this.dataUserProfile.totalMoneySpent + parseFloat(this.historyOfPurchaseTransactions[i].money);
+        }
       },
-      restOfMoneySpentFromTransactionDone(){
-        this.dataUserProfile.userWallet -= this.newVol.money;
-        this.dataUserProfile.totalMoneySpent += this.newVol.money;
+      totalUserAvailableMoney(){
+        this.dataUserProfile.userMoneyRegister = this.dataUserProfile.totalMoneyEarned - this.dataUserProfile.totalMoneySpent;
+
+        this.calculatingUserCoinsAvailable();
+      },
+      calculatingUserCoinsAvailable(){
+        this.sumOfCoins();
+        this.restOfCoins();
       },
       sumOfCoins(){
-        for(let i = 0; i < this.dataUserProfile.unitCoinAmount.length; i++){
-          if(this.newVol.crypto_code == 'bitcoin'){
-            this.dataUserProfile.unitCoinAmount[i] += parseFloat(this.newVol.crypto_amount);
+        for(let i = 0; i < this.historyOfPurchaseTransactions.length; i++){
+          if(this.historyOfPurchaseTransactions[i].crypto_code == 'bitcoin'){
+            this.dataUserProfile.unitCoinAmount.bitcoinAmount += parseFloat(this.historyOfPurchaseTransactions[i].crypto_amount);
           }
-          else if (this.newVol.crypto_code == 'dogecoin'){
-            this.dataUserProfile.unitCoinAmount[i] += parseFloat(this.newVol.crypto_amount);
+          else if (this.historyOfPurchaseTransactions[i].crypto_code == 'dogecoin'){
+            this.dataUserProfile.unitCoinAmount.dogecoinAmount += parseFloat(this.historyOfPurchaseTransactions[i].crypto_amount);
           }
-          else if (this.newVol.crypto_code == 'ethereum'){
-            this.dataUserProfile.unitCoinAmount[i] += parseFloat(this.newVol.crypto_amount);
+          else if (this.historyOfPurchaseTransactions[i].crypto_code == 'ethereum'){
+            this.dataUserProfile.unitCoinAmount.ethereumAmount += parseFloat(this.historyOfPurchaseTransactions[i].crypto_amount);
           }
-          else if (this.newVol.crypto_code == 'litecoin'){
-            this.dataUserProfile.unitCoinAmount[i] += parseFloat(this.newVol.crypto_amount);
+          else if (this.historyOfPurchaseTransactions[i].crypto_code == 'litecoin'){
+            this.dataUserProfile.unitCoinAmount.litecoinAmount += parseFloat(this.historyOfPurchaseTransactions[i].crypto_amount);
           }
-          else if (this.newVol.crypto_code == 'solana'){
-            this.dataUserProfile.unitCoinAmount[i] += parseFloat(this.newVol.crypto_amount);
+          else if (this.historyOfPurchaseTransactions[i].crypto_code == 'solana'){
+            this.dataUserProfile.unitCoinAmount.solanaAmount += parseFloat(this.historyOfPurchaseTransactions[i].crypto_amount);
           }
-          else if (this.newVol.crypto_code == 'usdcd'){
-            this.dataUserProfile.unitCoinAmount[i] += parseFloat(this.newVol.crypto_amount);
+          else if (this.historyOfPurchaseTransactions[i].crypto_code == 'usdcd'){
+            this.dataUserProfile.unitCoinAmount.usdcAmount += parseFloat(this.historyOfPurchaseTransactions[i].crypto_amount);
           }
         }
       },
       restOfCoins(){
-        for(let i = 0; i < this.dataUserProfile.unitCoinAmount.length; i++){
-          if(this.newVol.crypto_code == 'bitcoin'){
-            this.dataUserProfile.unitCoinAmount[i] -= parseFloat(this.newVol.crypto_amount);
+        for(let i = 0; i < this.historyOfSaleTransactions.length; i++){
+          if(this.historyOfSaleTransactions[i].crypto_code == 'bitcoin'){
+            this.dataUserProfile.unitCoinAmount.bitcoinAmount -= parseFloat(this.historyOfSaleTransactions[i].crypto_amount);
           }
-          else if (this.newVol.crypto_code == 'dogecoin'){
-            this.dataUserProfile.unitCoinAmount[i] -= parseFloat(this.newVol.crypto_amount);
+          else if (this.historyOfSaleTransactions[i].crypto_code == 'dogecoin'){
+            this.dataUserProfile.unitCoinAmount.dogecoinAmount -= parseFloat(this.historyOfSaleTransactions[i].crypto_amount);
           }
-          else if (this.newVol.crypto_code == 'ethereum'){
-            this.dataUserProfile.unitCoinAmount[i] -= parseFloat(this.newVol.crypto_amount);
+          else if (this.historyOfSaleTransactions[i].crypto_code == 'ethereum'){
+            this.dataUserProfile.unitCoinAmount.ethereumAmount -= parseFloat(this.historyOfSaleTransactions[i].crypto_amount);
           }
-          else if (this.newVol.crypto_code == 'litecoin'){
-            this.dataUserProfile.unitCoinAmount[i] -= parseFloat(this.newVol.crypto_amount);
+          else if (this.historyOfSaleTransactions[i].crypto_code == 'litecoin'){
+            this.dataUserProfile.unitCoinAmount.litecoinAmount -= parseFloat(this.historyOfSaleTransactions[i].crypto_amount);
           }
-          else if (this.newVol.crypto_code == 'solana'){
-            this.dataUserProfile.unitCoinAmount[i] -= parseFloat(this.newVol.crypto_amount);
+          else if (this.historyOfSaleTransactions[i].crypto_code == 'solana'){
+            this.dataUserProfile.unitCoinAmount.solanaAmount -= parseFloat(this.historyOfSaleTransactions[i].crypto_amount);
           }
-          else if (this.newVol.crypto_code == 'usdcd'){
-            this.dataUserProfile.unitCoinAmount[i] -= parseFloat(this.newVol.crypto_amount);
+          else if (this.historyOfSaleTransactions[i].crypto_code == 'usdcd'){
+            this.dataUserProfile.unitCoinAmount.usdcAmount -= parseFloat(this.historyOfSaleTransactions[i].crypto_amount);
           }
         }
       }
