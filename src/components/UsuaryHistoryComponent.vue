ead8c26f052b55b85da2fc361f2407633e3ecda8
Christian Buscarol <Chris-Emanuel-B@outlook.com>
2024-10-03 20:32:40 -0300

    Se separó y asignó las transacciones recibidas en unos arrays creados para su interacción, de paso se modificó el template con los nombres de dichos arrays junto a un índice para su renderizado, solo faltó comprobar estos últimos cambios ya que el ortiva de Docker le pintó tirar errores, así que cuando vuelva luego a probar rezaré para que todo funcione más o menos bien.

--- src/components/UsuaryHistoryComponent.vue
+++ src/components/UsuaryHistoryComponent.vue
@@ -4,11 +4,11 @@
       <!--En el 'Div' de acá abajo se renderizará el historial de compras de criptomonedas del usuario mediante el lado izquierdo de la pantalla.-->
       <div class="ShoppingHistoryBox">
         <h3>Table of purchased CriptoCoins...</h3>
-        <div v-for="(PurchaseT, index) in usuaryPurchaseTransactions" :key="index">
-          <h4>CriptoCoin purchased: {{ PurchaseT.criptoCoin }}</h4>
-          <h4>Money spent on the transaction: {{ PurchaseT.spentMoney }}</h4>
-          <h4>Amount of CriptoCoin sold: {{ PurchaseT.criptoCoinAmount }}</h4>
-          <h4>DateTime of purchase: {{ PurchaseT.dateTime }}</h4>
+        <div v-for="(UserP, index) in userPurchasesHistory" :key="index">
+          <h4>CriptoCoin purchased: {{ UserP.tittle }}</h4>
+          <h4>Money spent on the transaction: {{ UserP.money }}</h4>
+          <h4>Amount of CriptoCoin sold: {{ UserP.crypto_amount }}</h4>
+          <h4>DateTime of purchase: {{ UserP.dateTime }}</h4>
         </div>
         <!--Sector de botones para la configuración de las monedas compradas aquí abajo.-->
         <div class="PurchasedCoinsConfigButtons">
@@ -20,11 +20,11 @@
       <!--En el 'Div' de acá abajo se renderizará el historial de ventas de criptomonedas del usuario mediante el lado derecho de la pantalla.-->
       <div class="SalesHistoryBox">
         <h3>Table of CriptoCoins sold...</h3>
-        <div v-for="(SalesT, index) in usuarySalesTransactions" :key="index">
-          <h4>CriptoCoin sold: {{  }}</h4>
-          <h4>Money earned on the transaction: {{  }}</h4>
-          <h4>Amount of CriptoCoin purchased: {{  }}</h4>
-          <h4>DateTime of sale: {{ SalesT.dateTime }}</h4>
+        <div v-for="(UserS, index) in userSalesHistory" :key="index">
+          <h4>CriptoCoin sold: {{ UserS.tittle }}</h4>
+          <h3>Money earned on the transaction: {{ UserS.money }}</h3>
+          <h4>Amount of CriptoCoin purchased: {{ UserS.crypto_amount }}</h4>
+          <h4>DateTime of sale: {{ UserS.dateTime }}</h4>
         </div>
         <!--Sector de botones para la configuración de las monedas vendidas aquí abajo.-->
         <div class="CoinsSoldConfigButtons">
@@ -47,13 +47,27 @@
     },
     data(){
       return{
-        userTransactionHistory: []
+        userTransactionHistory: [],
+        userPurchasesHistory: [],
+        userSalesHistory: []
       }
     },
     methods: {
       userTransactionHistoryReceived(newVal){
+        this.userTransactionHistory = newVal.historyOfUserMovementsTransactions;
         console.log('Y la lista de transacciones recibida en el componente del historial es el siguiente: ');
-        console.log(newVal);
+        console.log(this.userTransactionHistory);
+        //this.separateTransactionByType();
+      },
+      separateTransactionByType(){
+        for(let i = 0; i < this.userTransactionHistory.length; i++){
+          if(this.userTransactionHistory[i].action == 'purchase' && isNaN(this.userTransactionHistory[i].money) != true){
+            this.userPurchasesHistory.push(this.userTransactionHistory[i]);
+          }
+          else if(this.userTransactionHistory[i].action == 'sell' && isNaN(this.userTransactionHistory[i].money) != true){
+            this.userSalesHistory.push(this.userTransactionHistory[i]);
+          }
+        }
       }
     },
     watch: {
