b300780725163888649d8bc32704e2f000734d47
Christian Buscarol <Chris-Emanuel-B@outlook.com>
2024-10-04 15:58:22 -0300

    Se modificó un array para que sea un objeto pensando que con este cambio funcionaría el paso de las transacciones hacia el componente del historial, pero el problema estaba en el componente en sí, así que que por más que se pase objeto o array el defecto no estaba en esta vista y se dejó el cambio así como quedo, y de paso se quitaron unos console.log().

--- src/views/UsuaryHistoryView.vue
+++ src/views/UsuaryHistoryView.vue
@@ -24,7 +24,7 @@
     data(){
       return{
         emitUserDataReceived: {},
-        emitUserTransactionsHistory: []
+        emitUserTransactionsHistory: {}
       }
     },
     methods: {
@@ -33,8 +33,6 @@
       },
       prepareUserTransactionsHistory(historyOfUserMovementsTransactions){
         this.emitUserTransactionsHistory = historyOfUserMovementsTransactions;
-        console.log('La lista de transacciones pasadas por la vista del historial es el siguiente: ');
-        console.log(this.emitUserTransactionsHistory);
       }
     },
     mounted(){
