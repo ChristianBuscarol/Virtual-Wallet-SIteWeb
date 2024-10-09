47bf0a57e9674e6c585b0d92133f3c1bd2573a5f
Christian Buscarol <Chris-Emanuel-B@outlook.com>
2024-10-08 21:37:47 -0300

    Se creó este componente cuyo formato es tipo Modal, y su funcionalidad es la de Eliminar o Modificar una transacción hecha y seleccionada por el usuario de turno, si bien está medio vacio ya que le falta elementos en el template y en su CSS, por lo menos dentro del script se preparó la prop a recibir mediante el ciclo de vida watch y una función para preparar dicha información recibida, ćomo se dijo, aún falta cosas por hacer, pero las bases se van haciendo y quedando de a poco.

---
+++ src/components/ModalTransactionModification.vue
@@ -0,0 +1,40 @@
+<template>
+
+</template>
+
+<script>
+  export default{
+    name: '',
+    props: {
+      receivedTransactionInfo: {
+        type: Object,
+        default: null
+      }
+    },
+    data(){
+      return{
+
+      }
+    },
+    methods: {
+      unitTransactionInfoReceived(newVal){
+        console.log('El newVal recibido en el componente del Modal para la modificación o eliminación de la transacción seleccionada es el siguiente: ');
+        console.log(newVal);
+      }
+    },
+    watch: {
+      receivedTransactionInfo: {
+        handler(newVal) {
+          if(newVal && Object.keys(newVal).length > 0){
+            this.unitTransactionInfoReceived(newVal);
+          }
+        },
+        inmediate: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
