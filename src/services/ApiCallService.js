7933d8e5d7ac5bc5784998986544e8c530da7d2d
Christian Buscarol <Chris-Emanuel-B@outlook.com>
2024-10-29 21:30:34 -0300

    Se empezó a trabajar con las 'url' que se encuentran en el documento de pasos a seguir del proyecto, pero un inconveniente sale cuando se trata de obtener todas la transacciones cuando el usuario ingresa su 'Id', no se como resolverlo, pero algo se me ocurrirá.

--- src/services/ApiCallService.js
+++ src/services/ApiCallService.js
@@ -3,16 +3,12 @@ import axios from "axios";
 /*
   baseURL: 'https://laboratorio3-f36a.restdb.io/rest/transactions',
 	headers: {'x-apikey': '64bdbb6f86d8c5e18ded91e3'}
-*/
-
-const apiClient = axios.create({
-	baseURL: 'http://localhost:8080/rest/transactions',
+  -----------------------------------------------------------------
+  baseURL: 'http://localhost:8080/rest/transactions',
   headers: {
     'Access-Control-Allow-Origin': '*'
   }
-});
-
-export default{
+  -----------------------------------------------------------------
   getUserTransactionsInfo(userId){
     const apiClientPurchase = axios.create({
       // TODO: Una vez cumplido los objetivos del proyecto como realiza el 'posteo' de las transacciones y otras cosas, arreglar el problema de la 'url' para que sea universal y no individual para cada consulta.
@@ -22,6 +18,19 @@ export default{
       }
     });
     return apiClientPurchase.get();
+  }
+*/
+
+const apiClient = axios.create({
+	baseURL: 'https://laboratorio3-5459.restdb.io/rest/transactions',
+	headers: {
+    'x-apikey': '64a57c2b86d8c50fe6ed8fa5'
+  }
+});
+
+export default{
+  getUserTransactionsInfo(userId){
+    return apiClient.get(`q={"user_id": "${userId}"}`);
   },
   postNewTransaction(transactionInfo){
     return apiClient.post('', transactionInfo);
