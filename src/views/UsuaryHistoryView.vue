19f4b875429ff502c70e4d8f3275199dd69b01f4
Christian Buscarol <Chris-Emanuel-B@outlook.com>
2024-11-19 12:28:12 -0300

    Se trabajó en el footer de la vista agregando contenido como un botón que te devuelve al catálogo de monedas y el correspondiente CSS de lo recién mencionado.

--- src/views/UsuaryHistoryView.vue
+++ src/views/UsuaryHistoryView.vue
@@ -9,7 +9,17 @@
     <ModalTransactionModification @refresh-the-view="refreshTheView" :receivedTransactionInfo="emitUserUnitTransactionInfo"></ModalTransactionModification>
   </body>
   <footer class="HistoryFooterBox">
-
+    <div class="FooterImageLeftBox">
+      <img src="../assets/PenguinOnComputer.webp" alt="A penguin in bicicle whit coins..." class="FooterGifs">
+    </div>
+    <div class="FooterCredits">
+      <h3>You have come to the final part of this proyect, i hope you enjoyed the interactions and i will see you soon whit more updates...</h3>
+      <h3>(If you want to go back to the coin catalogue page press the button right here below)</h3>
+      <button type="button" @click="btnCatalogueEntry()" class="btnGoToCataloguePage">Catalogue Page</button>
+    </div>
+    <div class="FooterImageRightBox">
+      <img src="../assets/PenguinOnBicicleWhitCoins.webp" alt="The Pug whit the most edit dance possible..." class="FooterGifs">
+    </div>
   </footer>
 </template>
 
@@ -44,6 +54,9 @@
       },
       refreshTheView(){
         window.location.reload();
+      },
+      btnCatalogueEntry(){
+        window.location.href = '/CoinCatalogueView';
       }
     },
     mounted(){
@@ -79,5 +92,54 @@
 
   .HeaderUserInfo{
     text-align: right;
+  }
+
+  footer{
+    background-color: #a39b9bd3;
+  }
+
+  .HistoryFooterBox{
+    display: flex;
+    justify-content: space-between; /* Distribuye los contenedores */
+    align-items: center;  /* Alinea verticalmente */
+    padding: 10px;
+  }
+
+  .FooterImageLeftBox{
+    text-align: left;
+  }
+
+  .FooterCredits{
+    text-align: center;
+  }
+
+  .FooterImageRightBox{
+    text-align: right;
+  }
+
+  .FooterGifs{
+    width: 200px;
+    height: 200px
+  }
+
+  .btnGoToCataloguePage{
+    width: 100px;
+    height: 40px;
+    cursor: pointer;
+    border-radius: 10px;  /* Bordes redondeados */
+    border: 1px solid blue;
+    background-color: white;
+    color: blue;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif; /* Fuente */
+    text-transform: uppercase; /* Convierte el texto a mayúsculas */
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.3); /* Sombra del botón */
+    transition: background-color 0.3s ease, box-shadow 0.3s ease;
+  }
+
+  .btnGoToCataloguePage:hover{
+    border: 1px solid white;
+    background-color: blue;
+    color: white;
+    box-shadow: 0px 6px 8px rgba(0, 0, 0, 0.5); /* Sombra más intensa al pasar el cursor */
   }
 </style>
